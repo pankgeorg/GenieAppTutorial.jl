217aef97164b616448edc2293d584fccd123399b
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliacomputing.com>
2022-04-20 00:14:41 +0300

    \!\?2

--- src/GenieAppTutorial.jl
+++ src/GenieAppTutorial.jl
@@ -3,15 +3,21 @@ module GenieAppTutorial
 using Genie, Logging, LoggingExtras
 import Sockets
 
-@info("Will initialize GenieAppTutorial")
+println("Will initialize GenieAppTutorial")
 
 function main()
   @info("Starting main")
   # Genie.genie(; context = @__MODULE__)
   Genie.load(context = @__MODULE__)
-  server = Sockets.listen(parse(Sockets.IPAddr, get(ENV, "HOST", "0.0.0.0")), parse(Int, get(ENV, "PORT", "8000")))
-  @info("\n\n\nWill run Genie\n\n\n")
-  Genie.run(; server = server)
+  @info("Loaded context")
+  try
+    server = Sockets.listen(parse(Sockets.IPAddr, get(ENV, "HOST", "0.0.0.0")), parse(Int, get(ENV, "PORT", "8000")))
+    @info("\n\n\nWill run Genie\n\n\n")
+    Genie.run(; server = server)
+  catch ex
+    @info "what?" (ex, catch_backtrace())
+    Genie.run(; server=Sockets.listen(parse(Sockets.IPAddr, get(ENV, "HOST", "127.0.0.1")), parse(Int, get(ENV, "PORT", "8000"))))
+  end
 end
 
 end
