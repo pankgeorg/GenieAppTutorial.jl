45d9e50792f3b26b84dd0b778666e4a592908476
Panagiotis Georgakopoulos <panagiotis.georgakopoulos@juliacomputing.com>
2022-04-20 00:41:12 +0300

    well?

--- src/GenieAppTutorial.jl
+++ src/GenieAppTutorial.jl
@@ -7,17 +7,13 @@ println("Will initialize GenieAppTutorial")
 
 function main()
   @info("Starting main")
-  # Genie.genie(; context = @__MODULE__)
-  Genie.load(context = @__MODULE__)
-  @info("Loaded context")
-  try
-    server = Sockets.listen(parse(Sockets.IPAddr, get(ENV, "HOST", "0.0.0.0")), parse(Int, get(ENV, "PORT", "8000")))
-    @info("\n\n\nWill run Genie\n\n\n")
-    Genie.run(; server = server)
-  catch ex
-    @info "what?" (ex, catch_backtrace())
-    Genie.run(; server=Sockets.listen(parse(Sockets.IPAddr, get(ENV, "HOST", "127.0.0.1")), parse(Int, get(ENV, "PORT", "8000"))))
-  end
+  
+  Core.eval(Main, :(const UserApp = $(@__MODULE__)))
+
+  Genie.genie(; context = @__MODULE__)
+
+  Core.eval(Main, :(const Genie = UserApp.Genie))
+  Core.eval(Main, :(using Genie))
 end
 
 end
